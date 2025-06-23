0f0b1b233a680f55f53b760bef5b721262e80dbc
watoma06 <95484621+watoma06@users.noreply.github.com>
2025-06-23 22:16:07 +0900

    Fix chart responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className="overflow-x-hidden">
       <body className={`${inter.className} overflow-x-hidden`}>{children}</body>
     </html>
   )
